f49844dabe57df6f1e3ee3e52700c61e27a383d0
back99 <tofoth@naver.com>
2022-12-11 20:27:09 +0900

    project commit

---
+++ src/main/java/com/mylog/controller/PostController.java
@@ -0,0 +1,14 @@
+package com.mylog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PostController {
+
+    @GetMapping("/posts")
+    public String get() {
+        return "Hello World";
+    }
+}
