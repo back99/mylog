99861b8e93d8683b027e558a9e5a0885a3f6757c
s049 <tofoth@naver.com>
2022-12-15 18:18:58 +0900

    feat: service test 추가

--- src/main/java/com/mylog/controller/PostController.java
+++ src/main/java/com/mylog/controller/PostController.java
@@ -7,9 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.lang.reflect.Field;
@@ -27,10 +25,12 @@ public class PostController {
 
     @PostMapping("/posts")
     public void post(@RequestBody @Valid PostCreate request) {
-        // Case1. 저장한 데이터 Entity -> response로 응답하기
-        // Case2. 저장한 데이터의 primary_id -> response로 응답하기
-        //         Client에서는 수신한 id를 글조회 API를 통해서 데이터를 수신 받음
-        // Case3. 응답 필요 없음 -> 클라이언트에서 모든 POST(글) 데이터 context를 잘 관리함
         postService.write(request);
+    }
+
+    @GetMapping("posts/{postId}")
+    public Post get(@PathVariable(name = "postId") Long id) {
+        Post post = postService.get(id);
+        return post;
     }
 }
